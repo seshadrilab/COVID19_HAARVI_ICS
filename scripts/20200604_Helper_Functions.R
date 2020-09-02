2079ca1628cabcdf963722f3ec6d488df1d7473f
malisas <malisas@users.noreply.github.com>
2020-09-02 13:47:09 -0700

    viz mean_gamma using ComplexHeatmap

--- scripts/20200604_Helper_Functions.R
+++ scripts/20200604_Helper_Functions.R
@@ -462,3 +462,125 @@ make_dotplot_for_COMPASS_run <- function(cr, run_name, output_folder=NA, current
     to_return
   }
 }
+
+# Like plot.COMPASSResult, but using the ComplexHeatmap package
+plot.COMPASSResult.ComplexHeatmap <- function(cr,
+                                              row_annotation = NULL,
+                                              cytokine_order_for_annotation = NULL,
+                                              dichotomize_by_cytokine = NULL,
+                                              dichotomize_by_cytokine_color = NULL,
+                                              row_annotation_colors = NULL,
+                                              staircase_cytokine_annotation = TRUE,
+                                              row_gap = unit(0, "in")) {
+  library(tidyverse)
+  library(ComplexHeatmap)
+  library(COMPASS)
+  library(RColorBrewer)
+  
+  if(!is.null(row_annotation) & is.null(row_annotation_colors)) { stop("row_annotation_colors must not be NULL if row_annotation is not NULL")}
+  
+  mean_gamma <- cr$fit$mean_gamma
+  cats <- as.data.frame(cr$fit$categories[, -ncol(cr$fit$categories),drop=FALSE]) # drop the "Counts" column
+  numMarkers <- ncol(cats)
+  rownames(cats) <- colnames(mean_gamma)
+  meta <- cr$data$meta
+  
+  # Filter the subsets to those where the average mean_gamma is greater than the threshold (default in heatmap and this function is 0.01)
+  compassSubsetsFiltered <- names(which(apply(mean_gamma, 2, function(x) { mean(x, na.rm = TRUE) }) > 0.01))
+  # And remove the subset with 0 positive markers
+  compassSubsetsFiltered <- compassSubsetsFiltered[lengths(regmatches(compassSubsetsFiltered, gregexpr("!", compassSubsetsFiltered))) != numMarkers]
+  
+  # Subset the cats rows to compassSubsetsFiltered, and put the columns in the order of cytokine_order_for_annotation
+  cats <- cats[compassSubsetsFiltered,]
+  if(!is.null(cytokine_order_for_annotation)) {
+    cats <- cats[, cytokine_order_for_annotation]
+  }
+  
+  # Before plotting, put the categories data frame rows in the desired order (columns were already re-ordered above)
+  # This is essentially the same code I added to the pheatmap function
+  cats <- cats[rev(do.call(order, cats)),,drop=FALSE]
+  # And order the cats df rows by degrees (number of cytokines in subset)
+  ckr<-apply(cats,1,function(x)sum(as.numeric(as.character(x))))
+  cats = cats[order(ckr),]
+  if(!is.null(dichotomize_by_cytokine)) {
+    # And then dichotomize the cats df rows so that all subsets containing the cytokine in dichotomize_by_cytokine (e.g. "IFNg") appear last
+    cats <- cats[order(cats[,dichotomize_by_cytokine]),]
+  }
+  
+  # Now re-order the columns of mean_gamma to match the rows of cats
+  mean_gamma <- mean_gamma[,rownames(cats)]
+  
+  # If dichotomizing and coloring by a cytokine, specify which cells in the cats matrix should be colored differently
+  if(!is.null(dichotomize_by_cytokine) & !is.null(dichotomize_by_cytokine_color)) {
+    current_cats_rownames <- rownames(cats)
+    cats <- cats %>% 
+      mutate_all(~ ifelse(. == 1 & !!as.symbol(dichotomize_by_cytokine) > 0, 2, .))
+    rownames(cats) <- current_cats_rownames
+  }
+  
+  # Now re-order the rows of mean_gamma by FunctionalityScore
+  FS_order <- order(FunctionalityScore(mean_gamma, n = numMarkers), decreasing=T)
+  mean_gamma <- mean_gamma[FS_order,]
+  # And make sure to update the metadata rows
+  meta <- meta[FS_order,]
+  stopifnot(all.equal(rownames(mean_gamma), rownames(meta)))
+  
+  # Now additionally order the rows of meta and then mean_gamma based on row_annotation
+  suppressWarnings(if(!is.null(row_annotation)) {
+    if(length(row_annotation) == 1) {
+      meta <- meta %>% arrange(!! rlang::sym(row_annotation))
+    } else if(length(row_annotation) > 1) {
+      meta <- meta %>% arrange(!!! rlang::syms(row_annotation))
+    }
+    mean_gamma <- mean_gamma[match(rownames(meta), rownames(mean_gamma)),]
+  })
+  
+  ht_opt$simple_anno_size = unit(2.5, "mm")
+  heatmap_main <- Heatmap(mean_gamma,
+                          cluster_rows = FALSE, 
+                          show_row_dend = FALSE, 
+                          cluster_columns = FALSE, cluster_column_slices = FALSE,
+                          row_split = suppressWarnings(if(!is.null(row_annotation)) { meta %>% dplyr::select(all_of(row_annotation)) } else { NULL }),
+                          border = "black",
+                          show_column_names = FALSE,
+                          show_row_names = FALSE, row_title = NULL,
+                          right_annotation = suppressWarnings(if(!is.null(row_annotation)) { 
+                            HeatmapAnnotation(df = meta %>% dplyr::select(all_of(row_annotation)),
+                                              col = row_annotation_colors,
+                                              which = "row", border = F,
+                                              show_annotation_name = F,
+                                              annotation_legend_param = list(border=F,
+                                                                             title_gp = gpar(fontsize = 13, fontface = "bold"),
+                                                                             labels_gp = gpar(fontsize = 12)))
+                          } else {
+                            NULL
+                          }),
+                          heatmap_legend_param = list(border=F, fontsize = 9),
+                          name="Response", use_raster = F,
+                          col = colorRampPalette(brewer.pal(9,"Purples"))(20),
+                          height = unit(5, "in"),
+                          row_gap = row_gap)
+  # draw(heatmap_main)
+  
+  heatmap_cats <- Heatmap(cats %>% dplyr::select(rev(everything())) %>% t(),
+                          cluster_rows = FALSE, 
+                          show_row_dend = FALSE, 
+                          cluster_columns = FALSE,
+                          border = T,
+                          show_column_names = FALSE,
+                          show_row_names = TRUE,
+                          row_names_gp = gpar(fontsize=14),
+                          row_names_side = "left",
+                          row_title = NULL,
+                          col = if(!is.null(dichotomize_by_cytokine) & !is.null(dichotomize_by_cytokine_color)) {
+                            c("white", "black", dichotomize_by_cytokine_color) } else { c("white", "black") },
+                          show_heatmap_legend = FALSE,
+                          rect_gp = gpar(col = "white", lwd = 1),
+                          height = unit(1.2, "in"))
+  # draw(heatmap_cats)
+  
+  # draw(heatmap_main %v% heatmap_cats, gap = unit(0.1, "in"), merge_legends = T) # heatmap_legend_side = "left"
+  # ht_opt(RESET = TRUE)
+  
+  heatmap_main %v% heatmap_cats
+}
